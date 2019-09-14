bdb7567d38f137a8c8f50de54a9d7d80764be529
jeffeaton <jeffrey.eaton@imperial.ac.uk>
2019-09-14 09:04:34 +0100

    update example survey datasets to match area hierarchy

--- tests/testthat/setup-load-data.R
+++ tests/testthat/setup-load-data.R
@@ -1,2 +1,11 @@
 
-data(mwi_areas)
+data(mwi_area_meta)
+data(mwi_area_hierarchy)
+data(mwi_area_names)
+data(mwi_area_boundaries)
+data(mwi_area_centers)
+
+data(mwi_population_agesex)
+data(mwi_survey_hiv_indicators)
+data(mwi_anc_testing)
+data(mwi_art_number)
