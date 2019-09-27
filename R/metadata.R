6a397129e6980fa537cc296df74743c7dcd3512e
Robert Ashton <r.ashton@imperial.ac.uk>
2019-09-27 15:04:14 +0100

    Add readme and tidy function

--- R/metadata.R
+++ R/metadata.R
@@ -1,31 +1,31 @@
-#' Get colour scale for a particular indicator and country
+get_plotting_metadata <- function(country) {
+  metadata <- get_metadata()
+  colour_scale <- get_colour_scale(country)
+  merge(x = metadata, y = colour_scale, by = "indicator")
+}
+
+#' Get colour scale and ranges for a particular country
 #'
-#' @param indicator Indicator to get scale for
 #' @param country Country to get scale for
 #'
 #' @return List of scale information including colour as a d3 scale chromatic
 #' function name, whether to invert the scale and a min and max value for the
 #' scale.
-#' @export
 #'
-get_colour_scale <- function(indicator, country) {
+#' @keywords internal
+get_colour_scale <- function(country) {
   scales <- naomi_read_csv(system.file("extdata", "meta", "colour_scales.csv",
                            package = "naomi"))
-  data <- scales[tolower(scales$country) == tolower(country) &
-                   scales$indicator == indicator, ]
+  data <- scales[tolower(scales$country) == tolower(country), ]
   if (nrow(data) == 0) {
-    stop(sprintf("Can't retrieve colour scale for country %s and indicator %s.
-Indicator and country combination not found in configuration.", country, indicator))
+    stop(sprintf(
+      "Can't retrieve colour scale for country %s. Country not found in configuration.",
+      country))
   }
-  if (nrow(data) > 1) {
-    stop(sprintf("Found more than one colour scale for country %s and indicator %s.
-Check configuration, each country and indicator combination should have 1 and only 1 entry.",
-                 country, indicator))
-  }
-  list(
-    colour = data$colour,
-    invert_scale = data$invert_scale,
-    min = data$min,
-    max = data$max
-  )
+  data
+}
+
+get_metadata <- function() {
+  naomi_read_csv(system.file("extdata", "meta", "metadata.csv",
+                                         package = "naomi"))
 }
--- tests/testthat/test-metadata.R
+++ tests/testthat/test-metadata.R
@@ -1,30 +1,23 @@
 context("metadata")
 
 test_that("can retrieve indicator colour scales for a country", {
-  scale <- get_colour_scale("prevalence", "Malawi")
-  expect_equal(scale$colour, "interpolateMagma")
-  expect_equal(scale$invert_scale, TRUE)
-  expect_equal(scale$min, 0)
-  expect_equal(scale$max, 0.5)
+  scale <- get_colour_scale("Malawi")
+  expect_true(all(c("country", "indicator", "colour", "min", "max",
+                    "invert_scale") %in% names(scale)))
+  expect_true(all(scale$country == "Malawi"))
 })
 
 test_that("getting scale for missing indicator or country returns error", {
-  expect_error(get_colour_scale("missing", "Malawi"),
-    "Can't retrieve colour scale for country Malawi and indicator missing.
-Indicator and country combination not found in configuration.")
-  expect_error(get_colour_scale("prevalence", "missing"),
-               "Can't retrieve colour scale for country missing and indicator prevalence.
-Indicator and country combination not found in configuration.")
+  expect_error(get_colour_scale("missing"),
+    "Can't retrieve colour scale for country missing. Country not found in configuration.")
 })
 
-test_that("return useful error if scale is misconfigured", {
-  mock_naomi_read_csv <- mockery::mock(data.frame(
-    country = c("Malawi", "Malawi"),
-    indicator = c("prevalence", "prevalence")
-  ))
-  with_mock("naomi:::naomi_read_csv" = mock_naomi_read_csv, {
-    expect_error(get_colour_scale("prevalence", "Malawi"),
-                 "Found more than one colour scale for country Malawi and indicator prevalence.
-Check configuration, each country and indicator combination should have 1 and only 1 entry.")
-  })
+test_that("can get plot metadata for a country", {
+  metadata <- get_plotting_metadata("Malawi")
+  expect_true(all(
+    c("indicator", "data_type", "plot_type", "value_column", "indicator_column",
+      "indicator_value", "name", "colour", "min", "max", "invert_scale") %in%
+      names(metadata)))
+  expect_true(all(unique(metadata$indicator) %in%
+                    c("art_coverage", "current_art",  "prevalence")))
 })
