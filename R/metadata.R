621249b0bf7b471455a4d026ede68e9cec9db760
Robert Ashton <r.ashton@imperial.ac.uk>
2019-10-21 14:34:10 +0100

    Update plotting metadata to return a default

--- R/metadata.R
+++ R/metadata.R
@@ -1,6 +1,7 @@
-#' Get plotting metadata for a particualr country
+#' Get plotting metadata for a particular country
 #'
-#' @param country Country to get metadata for
+#' @param country Country to get metadata for or blank for default
+#' configuration.
 #'
 #' @return List of plotting metadata about how to locate data for a specific
 #' indicator, data type and plot type. Also returns metadata about colour
@@ -10,28 +11,33 @@
 #'
 #' @examples
 #' get_plotting_metadata("Malawi")
-get_plotting_metadata <- function(country) {
+#' get_plotting_metadata()
+get_plotting_metadata <- function(country = "default") {
   metadata <- get_metadata()
   colour_scale <- get_colour_scale(country)
+  if (nrow(colour_scale) == 0) {
+    message(sprintf(
+      "Country %s not in metadata - returning default colour scales.", country))
+    colour_scale <- get_colour_scale()
+  }
   merge(x = metadata, y = colour_scale, by = "indicator")
 }
 
 #' Get colour scale and ranges for a particular country
 #'
-#' @param country Country to get scale for
+#' @param country Country to get scale for or return default scale if left
+#' blank.
 #'
 #' @return List of scale information including colour as a d3 scale chromatic
 #' function name, whether to invert the scale and a min and max value for the
 #' scale.
 #'
 #' @keywords internal
-get_colour_scale <- function(country) {
+get_colour_scale <- function(country = "default") {
   scales <- naomi_read_csv(system_file("extdata", "meta", "colour_scales.csv"))
   data <- scales[tolower(scales$country) == tolower(country), ]
-  if (nrow(data) == 0) {
-    stop(sprintf(
-      "Can't retrieve colour scale for country %s. Country not found in configuration.",
-      country))
+  if (nrow(data) == 0 && country == "default") {
+    stop(sprintf("Can't retrieve default colour scale. Check configuration."))
   }
   data
 }
--- tests/testthat/test-metadata.R
+++ tests/testthat/test-metadata.R
@@ -7,9 +7,28 @@ test_that("can retrieve indicator colour scales for a country", {
   expect_true(all(scale$country == "Malawi"))
 })
 
-test_that("getting scale for missing indicator or country returns error", {
-  expect_error(get_colour_scale("missing"),
-    "Can't retrieve colour scale for country missing. Country not found in configuration.")
+test_that("can retrieve default colour scales", {
+  scale <- get_colour_scale()
+  expect_true(all(c("country", "indicator", "colour", "min", "max",
+                    "invert_scale") %in% names(scale)))
+  expect_true(all(scale$country == "default"))
+})
+
+test_that("getting scale for missing country returns error empty data", {
+  missing <- get_colour_scale("missing")
+  expect_equal(names(missing),
+               c("country", "indicator", "colour", "min", "max","invert_scale"))
+  expect_equal(nrow(missing), 0)
+})
+
+test_that("default configuration missing throws an error", {
+  mock_naomi_read_csv <- mockery::mock(
+    data.frame(country = c("Malawi"), indicator = c("Test"),
+               stringsAsFactors = FALSE))
+  with_mock("naomi:::naomi_read_csv" = mock_naomi_read_csv, {
+    expect_error(get_colour_scale(),
+                 "Can't retrieve default colour scale. Check configuration.")
+  })
 })
 
 test_that("can get plot metadata for a country", {
@@ -19,6 +38,20 @@ test_that("can get plot metadata for a country", {
       "indicator_value", "name", "colour", "min", "max", "invert_scale") %in%
       names(metadata)))
   expect_true(all(unique(metadata$indicator) %in%
+                    c("art_coverage", "current_art",  "prevalence", "art_number",
+                      "incidence", "new_infections", "plhiv", "population",
+                      "recent", "vls")))
+})
+
+test_that("can get plot metadata for missing country with defaults", {
+  metadata <- testthat::evaluate_promise(get_plotting_metadata("missing"))
+  expect_equal(metadata$messages,
+    "Country missing not in metadata - returning default colour scales.\n")
+  expect_true(all(
+    c("indicator", "data_type", "plot_type", "value_column", "indicator_column",
+      "indicator_value", "name", "colour", "min", "max", "invert_scale") %in%
+      names(metadata$result)))
+  expect_true(all(unique(metadata$result$indicator) %in%
                     c("art_coverage", "current_art",  "prevalence", "art_number",
                       "incidence", "new_infections", "plhiv", "population",
                       "recent", "vls")))
