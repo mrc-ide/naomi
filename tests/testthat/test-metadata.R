4f23efab601fddbb70de5bdc89cf5701a03b4b3d
Jeff Eaton <jeffrey.eaton@imperial.ac.uk>
2019-11-29 12:47:37 +0000

    change [[ to [ to reference age_groups vector

--- tests/testthat/test-metadata.R
+++ tests/testthat/test-metadata.R
@@ -111,5 +111,5 @@ test_that("metadata is well formed", {
 test_that("can get 5 year age groups", {
   age_groups <- get_five_year_age_groups()
   expect_length(age_groups, 17)
-  expect_equal(age_groups[[1]], "00-04")
+  expect_equal(age_groups[1], "00-04")
 })
