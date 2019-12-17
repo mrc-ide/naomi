37e81c9185060711c1c248133e2e83a998830dd8
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2019-12-17 10:07:59 +0000

    Fix argument name

--- R/utils.R
+++ R/utils.R
@@ -7,7 +7,7 @@ naomi_read_csv <- function(...) {
 }
 
 readr_read_csv <- function(..., col_types = readr::cols()) {
-  readr::read_csv(..., col_types = cols_types)
+  readr::read_csv(..., col_types = col_types)
 }
 
 system_file <- function(...) {
