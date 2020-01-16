d6bb90baaf1b0fb65f45e8dfd1e9de932b7e29c4
Jeff Eaton <jeffrey.eaton@imperial.ac.uk>
2020-01-16 20:35:03 +0000

    fix missing calendar quarter in metadata

--- tests/testthat/test-outputs.R
+++ tests/testthat/test-outputs.R
@@ -3,7 +3,7 @@ context("model outputs")
 test_that("traidure hooks work in model outputs", {
 
   out_en <- output_package(a_fit_sample, a_naomi_mf)
-  expect_setequal(out_en$meta_period$quarter_label, c("March 2016", "September 2018"))
+  expect_setequal(out_en$meta_period$quarter_label, c("March 2016", "September 2018", "June 2019"))
   expect_setequal(out_en$meta_indicator$indicator_label[out_en$meta_indicator$indicator %in% c("art_coverage", "prevalence")],
                   c("ART coverage", "HIV prevalence"))
   expect_setequal(out_en$meta_indicator$description[out_en$meta_indicator$indicator %in% c("art_coverage", "prevalence")],
@@ -14,7 +14,7 @@ test_that("traidure hooks work in model outputs", {
 
   ## !!! TODO test need updating with French strings
   out_fr <- output_package(a_fit_sample, a_naomi_mf)
-  ## expect_setequal(out_fr$meta_period$quarter_label, c("<...> 2016", "<...> 2018"))
+  ## expect_setequal(out_fr$meta_period$quarter_label, c("<...> 2016", "<...> 2018", "<...> 2020"))
   expect_setequal(out_fr$meta_indicator$indicator_label[out_fr$meta_indicator$indicator %in% c("art_coverage", "prevalence")],
                   c("Prévalence du VIH", "Couverture ART"))
   ## expect_setequal(out_fr$meta_indicator$description[out_fr$meta_indicator$indicator %in% c("art_coverage", "prevalence")],
