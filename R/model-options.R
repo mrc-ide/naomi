8b6708bb175bb800dd06a91dfc89ebff382875bd
jeffeaton <jeffrey.eaton@imperial.ac.uk>
2020-01-24 11:30:08 +0000

    commit the file with the important stuff

--- R/model-options.R
+++ R/model-options.R
@@ -122,9 +122,13 @@ validate_model_options <- function(data, options) {
     }
   }
 
+  if (all(is.na(area_merged$spectrum_region_code))) {
+    stop(t_("SHAPE_SPECTRUM_REGION_ALL_NA"))
+  }
+         
   #' ## !! TODO: naomi::extract_pjnz_naomi() should be replaced with function that only extracts regions code
-  spec <- naomi::extract_pjnz_naomi(data$pjnz)
-
+  spec <- naomi::extract_pjnz_naomi(data$pjnz)  
+  
   missing_spectrum_regions <-
     !all(is.na(area_merged$spectrum_region_code) |
         area_merged$spectrum_region_code %in% spec$spectrum_region_code)
