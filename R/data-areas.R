269b074eece6d5eee4be5e10d9916f5646a5b1a0
Jeff Eaton <jeffrey.eaton@imperial.ac.uk>
2020-11-18 23:38:50 +0000

    remove area ID creation boundary check functions

--- R/data-areas.R
+++ R/data-areas.R
@@ -1,87 +1,8 @@
-#' Convert nested hierarchy from wide to long format
-#'
-#' @param x Wide format nested hierarchy.
-#'
-#' @export
-gather_areas <- function(x) {
-
-  val <- x %>%
-    dplyr::group_by(area_id = id0,
-             area_name = name0,
-             area_level = 0,
-             parent_area_id = NA,
-             spectrum_region_code =
-               if(n_distinct(id0) == n_distinct(id0, spectrum_region_code)) spectrum_region_code else NA
-             ) %>%
-    dplyr::summarise()
-
-  for(i in 1:6) {
-    if(exists(paste0("id", i), x) && !is.na(x[[paste0("id", i)]]))
-      val <- val %>%
-        rbind(
-          x %>%
-          dplyr::mutate(area_level = i) %>%
-          dplyr::rename(area_id = paste0("id", i),
-                        area_name = paste0("name", i),
-                        parent_area_id = paste0("id", i-1)) %>%
-          dplyr::group_by(area_id, area_name, area_level, parent_area_id,
-                          spectrum_region_code =
-                            if(n_distinct(area_id) == n_distinct(area_id, spectrum_region_code)) spectrum_region_code else NA) %>%
-          dplyr::summarise()
-        )
-  }
-
-  val %>%
-    ungroup
-}
-
-
 #' `ggplot2` theme for plotting maps
 #'
 #' @export
 th_map <- function(){
   ggplot2::theme_minimal() + ggplot2::theme(axis.text = element_blank())
-}
-
-
-#' Compare boundaries of two shapefiles by overlaying them
-#'
-#' @param sh1 is bottom shapefile with red boundaries
-#' @param sh2 is top shapefile with red boundaries
-#' @param aggregate whether to aggregate shapefiles
-#'
-#' @export
-compare_boundaries <- function(sh1, sh2 = NULL, aggregate = FALSE) {
-
-  if(aggregate){
-    sh1 <- dplyr::summarise(sh1)
-    if(!is.null(sh2))
-      sh2 <- dplyr::summarise(sh2)
-  }
-  p <- ggplot2::ggplot() + th_map()
-
-  p <- p + ggplot2::geom_sf(data = sh1, color = "red")
-  if(!is.null(sh2))
-    p <- p + ggplot2::geom_sf(data = sh2, color = "blue", fill = NA)
-
-  p
-}
-
-#' Check full and aggregated
-#'
-#' This function is useful for checking level of coarseness
-#' of a simplified versus raw shapefile and any slivers
-#' in a shapefile.
-#'
-#' @param sh1 Bottom shapefile with red boundaries
-#' @param sh2 Top shapefile with red boundaries
-#'
-#' @export
-check_boundaries <- function(sh1, sh2 = NULL){
-  gridExtra::grid.arrange(
-    compare_boundaries(sh1, sh2, aggregate = TRUE),
-    compare_boundaries(sh1, sh2, aggregate = FALSE),
-    nrow = 1)
 }
 
 #' Spread area hierarchy to wide format
