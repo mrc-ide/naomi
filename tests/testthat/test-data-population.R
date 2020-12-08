266ef69cfcef9f7f24c9317220663f470df37141
Robert Ashton <r.ashton@imperial.ac.uk>
2020-12-08 13:48:54 +0000

    Add calendar quarter to year helper

--- tests/testthat/test-data-population.R
+++ tests/testthat/test-data-population.R
@@ -9,4 +9,6 @@ test_that("test can convert between time ids", {
   expect_equal(calendar_quarter_to_quarter_id("CY2012Q4"), 452)
   expect_equal(quarter_id_to_calendar_quarter(449), "CY2012Q1")
   expect_equal(quarter_id_to_calendar_quarter(452), "CY2012Q4")
+  expect_equal(calendar_quarter_to_year("CY2012Q4"), 2012)
+  expect_equal(calendar_quarter_to_year("CY2012Q1"), 2012)
 })
