32e96c33cbaa03f1d8aa4a67cc97bc17d6c95c98
Robert Ashton <r.ashton@imperial.ac.uk>
2020-12-18 09:53:14 +0000

    Move calibration options back ot calibration page

--- tests/testthat/test-model-options.R
+++ tests/testthat/test-model-options.R
@@ -207,7 +207,7 @@ test_that("validation check for spectrum region code returns error", {
 test_that("can get model calibration options", {
   options <- get_model_calibration_options()
   expect_length(options, 1)
-  expect_true(any(grepl("Post model fit calibration", options)))
+  expect_true(any(grepl("Calibration options", options)))
 })
 
 test_that("can get model calibration options label from ID", {
