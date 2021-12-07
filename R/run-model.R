452890bcee9162e87bb77c884c7dfa9948b21dae
Jeff Eaton <jeffrey.eaton@imperial.ac.uk>
2021-12-07 17:21:26 +0000

    remove data paths from naomi_info

--- R/run-model.R
+++ R/run-model.R
@@ -650,8 +650,7 @@ naomi_info_packages <- function() {
 naomi_info <- function(data, options) {
   list("inputs.csv" = write_csv_string(naomi_info_input(data)),
        "options.yml" = yaml::as.yaml(options),
-       "packages.csv" = write_csv_string(naomi_info_packages()),
-       "data" = data)
+       "packages.csv" = write_csv_string(naomi_info_packages()))
 }
 
 ## Ensures data is of format
