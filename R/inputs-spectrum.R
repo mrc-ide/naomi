a911f8f8e51dab8ec05fbf4891fba665331810a2
Robert Ashton <r.ashton@imperial.ac.uk>
2022-11-24 18:55:10 +0000

    Sanitise region name

--- R/inputs-spectrum.R
+++ R/inputs-spectrum.R
@@ -435,7 +435,7 @@ read_spectrum_region_code <- function(pjnz) {
 read_spectrum_region_name <- function(pjnz) {
   pjn <- eppasm::read_pjn(pjnz)
   region_name <- pjn[which(pjn[, 1] == "<Projection Parameters - Subnational Region Name2>") + 2, 4]
-  if (isTRUE(region_name == "")) {
+  if (is.null(region_name) || !nzchar(region_name)) {
     region_name <- NA_character_
   }
   region_name
