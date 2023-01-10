7352a92c4fc67f015f4bf4f998a635d589aeb252
Robert Ashton <r.ashton@imperial.ac.uk>
2023-01-10 12:25:59 +0000

    Use with_mock function from mockr since testthat deprecated

--- tests/testthat/helper-testing.R
+++ tests/testthat/helper-testing.R
@@ -1,3 +1,7 @@
 expect_no_error <- function(object) {
   expect_error(object, NA)
 }
+
+with_mock <- function(..., .parent = parent.frame()) {
+  mockr::with_mock(..., .parent = .parent, .env = "naomi")
+}
