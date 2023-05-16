8de206eba733f3b4d9d5eddb9ec1f5781b0ad59f
Robert Ashton <r.ashton@imperial.ac.uk>
2023-05-16 15:48:45 +0100

    Fix test sizing

--- tests/testthat/test-01-run-model.R
+++ tests/testthat/test-01-run-model.R
@@ -307,7 +307,7 @@ test_that("model run can be calibrated", {
   ## Output has been calibrated
   expect_true(!is.null(calibrated_output$plot_data_path))
   calibrated_output_obj <- read_hintr_output(calibrated_output$model_output_path)
-  
+
   ## Total population outputs:
   ## * 33 age groups
   ## * 3 sexes
@@ -323,11 +323,12 @@ test_that("model run can be calibrated", {
   ## 12 = number of ANC age groups
   expect_equal(nrow(calibrated_output_obj$output_package$indicators),
                33 * 3 * 9 * (3 * 16 + 1 * 5 + 1 * 4) + 3 * 9 * 9 * 12)
-  
+
   ## Plot data output: T3 and T4 indicators not included -> fewer rows
-  plot_data_output <- read_hintr_output(calibrated_output$plot_data_path)  
+  ## Also excluded ANC indicator outputs and population
+  plot_data_output <- read_hintr_output(calibrated_output$plot_data_path)
   expect_equal(nrow(plot_data_output),
-               33 * 3 * 9 * (3 * 16 + 0 * 5 + 0 * 4) + 3 * 9 * 9 * 12)
+               33 * 3 * 9 * (3 * 15 + 0 * 5 + 0 * 4))
 
   expect_file_different(calibrated_output$model_output_path,
                         a_hintr_output$model_output_path)
