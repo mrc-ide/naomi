7c029a248af70e9922f8726ffd390ba9e18efff4
Mantra <mantrakusumgar@outlook.com>
2024-09-03 19:19:39 +0100

    make anc faster

--- R/input-time-series.R
+++ R/input-time-series.R
@@ -11,13 +11,17 @@
 ##'  year, quarter,calendar_quarter and art_current
 ##'
 ##' @export
-aggregate_art <- function(art, shape) {
+aggregate_art <- function(art, shape, drop_geometry = TRUE) {
 
   ## Check if shape is object or file path
-  if (!inherits(shape, "sf")) {
-    areas <- sf::read_sf(shape) |> sf::st_drop_geometry()
+  if (drop_geometry) {
+    if(!inherits(shape, "sf")) {
+      areas <- sf::read_sf(shape) |> sf::st_drop_geometry()
+    } else {
+      areas <- shape |> sf::st_drop_geometry()
+    }
   } else {
-    areas <- shape |> sf::st_drop_geometry()
+    areas <- shape
   }
 
   ## Check if art is object or file path
@@ -144,7 +148,7 @@ prepare_input_time_series_art <- function(art, shape) {
 
   ## Recursively aggregate ART data up from lowest level of programme data provided
   # Levels to aggregate up from
-  art_long <- aggregate_art(art, shape)
+  art_long <- aggregate_art(art, shape, drop_geometry = FALSE)
   sex_level <- unique(art_long$sex)
   age_level <- unique(art_long$age_group)
   admin_level <- max(art_long$area_level)
@@ -307,6 +311,8 @@ prepare_input_time_series_art <- function(art, shape) {
 ##'
 ##' @param anc Path to file containing ANC data or ANC data object
 ##' @param shape Path to file containing geojson areas data or areas data object
+##' @param drop_geometry Setting this to FALSE will skip dropping geometry on
+##'   the shape file (default TRUE)
 ##'
 ##' @return Aggregated ANC data containing columns area_id, area_name, area_level,
 ##' area_level_label, sex,age_group, time_period, year, quarter, calendar_quarter,
@@ -314,16 +320,20 @@ prepare_input_time_series_art <- function(art, shape) {
 ##' births_clients_ratio
 
 ##' @export
-aggregate_anc <- function(anc, shape) {
+aggregate_anc <- function(anc, shape, drop_geometry = TRUE) {
 
   ## Recursively aggregate ANC data up from lowest level of programm data provided
   # Level to aggregate from
 
   ## Check if shape is object or file path
-  if(!inherits(shape, "sf")) {
-    areas <- sf::read_sf(shape) |> sf::st_drop_geometry()
+  if (drop_geometry) {
+    if(!inherits(shape, "sf")) {
+      areas <- sf::read_sf(shape) |> sf::st_drop_geometry()
+    } else {
+      areas <- shape |> sf::st_drop_geometry()
+    }
   } else {
-    areas <- shape |> sf::st_drop_geometry()
+    areas <- shape
   }
 
   ## Check if anc is object or file path
@@ -336,53 +346,72 @@ aggregate_anc <- function(anc, shape) {
                  "anc_tested", "anc_tested_pos", "anc_known_neg", "births_facility")
   cols_keep <- intersect(cols_list, colnames(anc))
 
-  anc <- anc |>
+  # make sure the ANC data is the correct shape, note that all the
+  # variables in cols_list are requried expect births_facility
+  clean_anc <- anc |>
     dplyr::select(area_id, age_group, year, dplyr::any_of(cols_list))
 
-  anc_testing <- anc |>
-    dplyr::left_join( dplyr::select(areas, area_id, area_level), by = "area_id")
+  # initialise aggregated anc with the max admin level (most fine grained)
+  # and fill in and missing rows - if a year has max admin level n (these can be
+  # different per year) then we fill in values for any missing areas at admin
+  # level n with NAs
+  agg_anc <- clean_anc |>
+    dplyr::left_join(dplyr::select(areas, area_id, area_level), by = "area_id") %>%
+    dplyr::group_by(year) %>%
+    # summarise to table with columns year, area_level (max area level for this year)
+    # and age_group
+    dplyr::summarize(
+      area_level = dplyr::first(area_level),
+      age_group = dplyr::first(age_group)
+    ) %>%
+    # expand each year row to multiple rows with all area_ids for that admin level
+    # this is the complete list of area_ids that we need
+    dplyr::left_join(
+      areas %>% dplyr::select(area_id, area_level), by = "area_level"
+    ) %>%
+    # left join complete list of area_ids with our potentially missing area_ids in
+    # clean_anc to get rows of NAs if an area_id is missing
+    dplyr::left_join(clean_anc, by = c("year", "area_id", "age_group")) |>
+    dplyr::select(area_id, age_group, year, area_level, dplyr::any_of(cols_list))
 
-  # Split data by year and aggregate from lowest level available
-  aggregate_anc_by_level <- function(anc_testing){
-    ## Recursively aggregate ANC data up from lowest level of programme data provided
-    # Level to aggregate from
-    anc_level <- max(anc_testing$area_level)
+  area_with_parent_ids <- areas |>
+    dplyr::select(area_id, parent_area_id)
 
-    higher_areas <- areas %>%
-      dplyr::filter(area_level <= anc_level) %>%
-      dplyr::select(area_id, area_level, parent_area_id)
+  max_area_level <- max(agg_anc$area_level)
 
-    agg_anc <- anc_testing %>%
-      dplyr::left_join(higher_areas, by = c("area_id", "area_level"))
-
-    for (level in anc_level:1) {
-      agg_anc <- agg_anc %>%
-        dplyr::filter(area_level == level) %>%
-        dplyr::group_by(parent_area_id) %>%
-        dplyr::summarise_at(dplyr::vars(dplyr::all_of(cols_keep)),
-                            ~sum(., na.rm = TRUE),
-                            .groups = "drop") %>%
-        dplyr::rename(area_id = parent_area_id) %>%
-        dplyr::left_join(higher_areas, by = "area_id") %>%
-        dplyr::bind_rows(agg_anc)
-    }
-
-    year <- dplyr::cur_group() %>% dplyr::pull(year)
-
-    agg_anc %>%
-      dplyr::mutate(year = year) %>%
-      tidyr::fill(age_group, year, .direction = "up")
+  # every iteration of the loop aggregates the area up one area_level and
+  # adds all those rows to agg_anc
+  for (level in max_area_level:1) {
+    agg_anc <- agg_anc |>
+      dplyr::filter(area_level == level) |>
+      dplyr::left_join(area_with_parent_ids, by = "area_id") |>
+      # TODO test age group in group_by vs summarise
+      dplyr::group_by(parent_area_id, year) |>
+      # > year and age_group assumed to be the same as we aggregate up so
+      #   just take the first value
+      # > area_level is one less since we are aggregating to the parent
+      # > sum across all the other numeric values ignoring NAs
+      dplyr::summarise(
+        year = dplyr::first(year),
+        age_group = dplyr::first(age_group),
+        area_level = dplyr::first(area_level) - 1,
+        dplyr::across(dplyr::any_of(cols_list), ~sum(.x, na.rm = TRUE)),
+        .groups = "drop"
+      ) |>
+      dplyr::rename(area_id = parent_area_id) |>
+      dplyr::bind_rows(agg_anc)
   }
 
-  anc_long <- anc_testing %>%
-    dplyr::group_by(year) %>%
-    dplyr::reframe(aggregate_anc_by_level(dplyr::cur_data())) %>%
+  # add in extra columns and sort
+  anc_long <- agg_anc |>
     dplyr::left_join(
-      areas %>% dplyr::select(area_id, area_name, area_level_label, area_sort_order),
+      areas |> dplyr::select(
+        area_id, area_name, area_level_label, area_sort_order, parent_area_id
+      ),
       by = "area_id"
-    ) %>%
+    ) |>
     dplyr::mutate(time_period = as.character(year), quarter = "Q4", sex = "female",
-                  calendar_quarter = paste0("CY", time_period, quarter)) %>%
+                  calendar_quarter = paste0("CY", time_period, quarter)) |>
     dplyr::arrange(year, area_sort_order)
 
   anc_long$area_hierarchy <- build_hierarchy_label(anc_long)
@@ -413,7 +442,7 @@ prepare_input_time_series_anc <- function(anc, shape) {
   }
 
   ## Shape data for plot
-  anc_long <- aggregate_anc(anc, shape)
+  anc_long <- aggregate_anc(anc, areas, drop_geometry = FALSE)
 
   anc_plot_data_long <- anc_long |>
     dplyr::mutate(
@@ -423,7 +452,7 @@ prepare_input_time_series_anc <- function(anc, shape) {
       anc_art_among_known = anc_already_art / anc_known_pos,
       anc_art_coverage = anc_already_art / anc_total_pos,
       births_clients_ratio = births_facility / anc_clients
-      ) |>
+    ) |>
     dplyr::select(area_id, area_name, area_level, area_level_label, parent_area_id,
                   area_sort_order, age_group,  time_period, year, quarter,
                   calendar_quarter, anc_clients, anc_tested, anc_tested_pos,
@@ -437,31 +466,44 @@ prepare_input_time_series_anc <- function(anc, shape) {
 
   # Tag data with NAs at the lowest admin level
   anc_level <- max(anc_plot_data_long$area_level)
-  hierarchy_wide <- spread_areas(areas |> dplyr::filter(area_level <= anc_level))
 
+  cols_list <- c("anc_clients", "anc_known_pos", "anc_already_art",
+                 "anc_tested", "anc_tested_pos", "anc_known_neg", "births_facility")
+
+  # initialise missing_map with values that are missing, these will only show up
+  # at the max admin level per year
   missing_map <- anc_plot_data_long |>
-    # For edge cases where data is provided at different admin levels for
-    # different years (get max area level by year)
-    dplyr::select(calendar_quarter, area_level) |>
-    dplyr::group_by(calendar_quarter) |>
-    dplyr::summarise(area_level = max(area_level)) |>
-    # Select lowest admin level for each year
-    dplyr::left_join(anc_plot_data_long |>
-                       dplyr::select(area_id, area_name, area_level, calendar_quarter, plot, value),
-                     multiple = "all", by = dplyr::join_by(calendar_quarter, area_level)) |>
-    # Joint to wide hierarchy
-    dplyr::left_join(hierarchy_wide, by = dplyr::join_by(area_id)) |>
-    # Filter for districts with missing value
-    dplyr::filter(is.na(value)) |>
-    dplyr::select(missing = area_id, dplyr::everything()) |>
-    # Get into long format
-    tidyr::pivot_longer(cols = dplyr::starts_with("area_id"), values_to = "area_id") |>
-    # Aggregate missing observations by area_id at all levels
-    dplyr::group_by(area_id, plot, calendar_quarter) |>
-    dplyr::summarise(missing_ids = list(missing), .groups = "drop")
+    dplyr::select(area_id, year, value, plot, area_level) |>
+    # find NAs, also check it isn't a NaN because these can appear in some
+    # derived columns where we divide by 0
+    dplyr::filter(is.na(value) & !is.nan(value)) |>
+    dplyr::select(-value) |>
+    dplyr::mutate(missing_ids = as.list(area_id))
+  
+  area_with_parent_ids <- areas |>
+    dplyr::select(area_id, parent_area_id)
+
+  # same idea as in aggregate_anc, every iteration of the loop aggregates
+  # up one admin level
+  for (level in anc_level:1) {
+    missing_map <- missing_map |>
+      dplyr::filter(area_level == level) |>
+      dplyr::left_join(area_with_parent_ids, by = "area_id") |>
+      dplyr::group_by(parent_area_id, year, plot) |>
+      # > missing_ids merge the two lists together
+      # > area_level decrease area_level by one because we aggregate
+      #   up to the parent
+      dplyr::summarise(
+        missing_ids = list(unlist(missing_ids, FALSE, FALSE)),
+        area_level = dplyr::first(area_level) - 1,
+        .groups = "drop"
+      ) |>
+      dplyr::rename(area_id = parent_area_id) |>
+      dplyr::bind_rows(missing_map)
+  }
 
   df_final <- anc_plot_data_long |>
-    dplyr::left_join(missing_map, by = dplyr::join_by(area_id, calendar_quarter, plot)) |>
+    dplyr::left_join(missing_map, by = dplyr::join_by(area_id, year, plot, area_level)) |>
     dplyr::mutate(value = tidyr::replace_na(value, 0), missing = NULL)
 
   return(df_final)
