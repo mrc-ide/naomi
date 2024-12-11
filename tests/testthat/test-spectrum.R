6fcb1cb0f5c65ebdf8c238216a9456222861af7d
rtesra <70330391+rtesra@users.noreply.github.com>
2024-12-11 17:43:40 +0200

    fix failing spectrum test

--- tests/testthat/test-spectrum.R
+++ tests/testthat/test-spectrum.R
@@ -206,7 +206,8 @@ test_that("ART adjustments properly extracted from Spectrum", {
 
   # Test ART adjustments correctly read from test file with
   expect_equal(colnames(spec_art), c("sex", "age_group", "year",
-                                     "art_dec31_reported", "art_dec31_attend","art_dec31_reside"))
+                                     "art_dec31_reported", "art_dec31_attend",
+                                     "art_dec31_reside", "art_dec31"))
   # Adults
   expect_setequal(spec_art[166,], c("male", "Y015_999", "2022", "450799", "387903", "377903"))
   expect_setequal(spec_art[167,], c("female", "Y015_999", "2022", "857994", "719095", "707095"))
@@ -214,3 +215,4 @@ test_that("ART adjustments properly extracted from Spectrum", {
   expect_setequal(spec_art[52,], c("both", "Y000_014", "2021", "59853", "49368", "44368"))
 
 })
+
