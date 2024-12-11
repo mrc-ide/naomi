9bffe8f1aa78d637e37ca131d95ff7613c599350
Rob Ashton <robertashton94@gmail.com>
2024-12-11 21:14:48 +0000

    Remove failing navigator checklist test

--- tests/testthat/test-downloads.R
+++ tests/testthat/test-downloads.R
@@ -248,26 +248,6 @@ test_that("spectrum download can be created", {
   expect_equal(dir(tmp), "info")
   expect_equal(dir(file.path(tmp, "info")), names(info))
 
-
-  ## # UNAIDS Navigator Checklist checks
-  navigator_checklist <- utils::read.csv(unz(out$path, "info/unaids_navigator_checklist.csv"))
-
-
-  expect_equal(names(navigator_checklist),
-               c("NaomiCheckPermPrimKey", "NaomiCheckDes", "TrueFalse"))
-
-  checklist_primkeys <- c( "Package_has_all_data","Opt_recent_qtr","Opt_future_proj_qtr",
-                           "Opt_area_ID_selected","Opt_calendar_survey_match","Opt_recent_survey_only",
-                           "Opt_ART_coverage","Opt_ANC_data","Opt_ART_data",
-                           "Opt_ART_attendance_yes","Model_fit","Cal_Population",
-                           "Cal_PLHIV","Cal_ART","Cal_KOS",
-                           "Cal_new_infections","Cal_method" )
-  expect_equal(navigator_checklist$NaomiCheckPermPrimKey, checklist_primkeys)
-  expect_true(all(navigator_checklist$TrueFalse %in% c(TRUE, FALSE)))
-  ## Check tradiure translation hooks worked
-  expect_true("Calibration - method is logistic" %in% navigator_checklist$NaomiCheckDes)
-
-
   outputs <- read_output_package(out$path)
   expect_true(
     all(c("area_level", "area_level_label", "area_id", "area_name", "parent_area_id",
